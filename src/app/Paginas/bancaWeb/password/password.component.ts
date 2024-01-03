16b72d3dc8c9d2e74eece2b141b364e292f99ca6
edisonjt <eddyt1999@gmail.com>
2024-01-03 15:47:14 -0500

    feat: :sparkles: change password

--- src/app/Paginas/bancaWeb/password/password.component.ts
+++ src/app/Paginas/bancaWeb/password/password.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FlujoDatosService } from 'src/app/Servicios/flujo-datos.service';
+import { LoginService } from 'src/app/Servicios/login.service';
 
 @Component({
   selector: 'app-password',
@@ -7,5 +9,38 @@ import { Router } from '@angular/router';
   styleUrls: ['./password.component.css']
 })
 export class PasswordComponent {
-  constructor(private router: Router) { }
+  antiguaContrasena: string = '';
+  contrasena: string = '';
+  newContrasena: string = '';
+
+  constructor(private router: Router,
+    private loginService: LoginService,
+    private flujoDatosService: FlujoDatosService,
+  ) { }
+
+  validarContrasena(): void {
+    if(this.contrasena === this.newContrasena){    
+      this.actualizarContrasena();
+    }else{
+      alert("Las contraseñas no coinciden")
+    }
+  }
+
+  actualizarContrasena(): void {
+    const contrasena = {
+      idCliente: this.flujoDatosService.getId(),
+      contrasenaAntigua: this.antiguaContrasena,
+      nuevaContrasena: this.newContrasena,
+    }
+    this.loginService.actualizarContrasena(contrasena).subscribe(
+      data => {
+        console.log("Contraseña actualizada")
+        alert("Contraseña actualizada")
+      },
+      error => {
+        console.error("Error al actualizar contraseña", error)
+        alert("Error al actualizar la contraseña")
+      }
+    )
+  }
 }
