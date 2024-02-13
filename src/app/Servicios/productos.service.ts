f808d338e43144a9d310f165325807674ece7611
Sebastian Jaramillo <hsjj2000@gmail.com>
2024-02-13 13:54:40 -0500

    Cambios transferencias

--- src/app/Servicios/productos.service.ts
+++ src/app/Servicios/productos.service.ts
@@ -6,14 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductosService {
-
-<<<<<<< HEAD
   private cuentasAhorroApi = 'http://34.176.119.102:9090/api/v1/cuentas/clientes/';
   private creditosApi = 'http://34.176.119.102:9090/api/v1/creditos/clientes/';
-=======
-  private cuentasAhorroApi = 'http://35.192.152.130:8089/api/v1/cuentas/clientes/';
-  private creditosApi = 'http://34.125.114.60:8080/credito/buscar-codigo-cliente';
->>>>>>> f040000bcc7d5248eceeb00ebe2b7509f0ec29a3
 
   constructor(private http: HttpClient) { }
 
